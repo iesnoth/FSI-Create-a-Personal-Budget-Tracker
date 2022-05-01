a74026a6fda357ce8240d2db051077464e8d659d
Victoria Beasley <iesnoth91@gmail.com>
2022-05-01 17:32:33 -0500

    review, experimenting with function

--- index.js
+++ index.js
@@ -3,3 +3,24 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+for(let i =0; i <weeklyExpenseQuestions.length; i++){
+    let answer= parseInt(window.prompt(weeklyExpenseQuestions[i]))
+    console.log(answer);
+    weeklyExpenses = weeklyExpenses + answer;
+}
+
+function iterateOverQuestions(array){
+for(let i =0; i <monthlyExpenseQuestions.length; i++){
+    let answer= parseInt(window.prompt(monthlyExpenseQuestions[i]))
+    console.log(answer);
+    monthlyExpenses = monthlyExpenses + answer;
+}
+}
+
+iterateOverQuestions(annualExpenseQuestions)
+/*for(let i =0; i <annualExpenseQuestions.length; i++){
+    let answer= parseInt(window.prompt(annualExpenseQuestions[i]))
+    console.log(answer);
+    annualExpenses = annualExpenses + answer;
+}
+*/
