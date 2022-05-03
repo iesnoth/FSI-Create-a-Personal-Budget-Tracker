41cce58b7cac84943ebbc34b3fe5633333a72d67
Victoria Beasley <iesnoth91@gmail.com>
2022-05-03 18:52:55 -0500

    deletion

--- index.js
+++ index.js
@@ -4,21 +4,11 @@ let annualExpenses = iterateOverQuestions(annualExpenseQuestions)
 
 // Your Code Here
 function iterateOverQuestions(array){
-    let sum = 0
+    let sum = 0;
     for(let i =0; i <array.length; i++){
     let answer= parseInt(window.prompt(array[i]))
     console.log(answer);
     sum += answer;
     }
-    return sum
-}
-
-iterateOverQuestions(weeklyExpenseQuestions)
-
-function iterateOverQuestions(array){
-for(let i =0; i <monthlyExpenseQuestions.length; i++){
-    let answer= parseInt(window.prompt(monthlyExpenseQuestions[i]))
-    console.log(answer);
-    monthlyExpenses = monthlyExpenses + answer;
-}
+    return sum;
 }
